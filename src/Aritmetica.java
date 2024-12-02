c93b8d230a34128f839e183a2a8ad26e18141d3c
m_adriani <m.adriani@jdk.it>
2024-12-02 12:55:06 +0100

    aggiunta la funzionalità di somma

---
+++ src/Aritmetica.java
@@ -0,0 +1,8 @@
+public class Aritmetica {
+
+    public static void stampaSomma(int x, int y) {
+        int somma = x + y;
+        System.out.println("la somma tra x e y è: " + somma);
+    }
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Ciao brutti da Trastevere");
+        Aritmetica.stampaSomma(10, 7);
     }
 
 }
